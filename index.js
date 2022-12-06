756b075cd95d5f513be55741784e6422049e6c05
NicolasFontana <nico.fontana12@gmail.com>
2022-12-06 16:39:27 -0300

    finished 'file metadata project'

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const multer  = require('multer')
 var cors = require('cors');
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +13,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + process.env.PORT)
 });
